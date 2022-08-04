d7e15d4229089eaae13035aaaa7ff456a68ca040
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-08-03 20:55:50 -0700

    fully migrated visualization

--- phot_functions.py
+++ phot_functions.py
@@ -5,7 +5,6 @@ import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
-from astropy.nddata import Cutout2D
 from astropy.stats import SigmaClip, sigma_clipped_stats, gaussian_fwhm_to_sigma
 from astropy.convolution import Gaussian2DKernel, convolve
 from astropy.timeseries import TimeSeries
@@ -18,15 +17,6 @@ from photutils.background import Background2D, SExtractorBackground
 from photutils.segmentation import detect_sources, deblend_sources, SourceCatalog
 from photutils.aperture import CircularAperture, CircularAnnulus, ApertureStats
 from scipy.signal import savgol_filter
-
-import mpld3
-import matplotlib.pylab as plt
-from matplotlib.patches import Rectangle, Circle
-from matplotlib.ticker import MaxNLocator
-from matplotlib.colors import Normalize
-import matplotlib.dates as mdates
-from astropy.visualization import ZScaleInterval, MinMaxInterval, PowerStretch, LogStretch
-from astropy.visualization.mpl_normalize import ImageNormalize
 
 from visu_functions import *
 
@@ -303,127 +293,18 @@ def LCs_visualizer(directory, visu_dir, mode='simple', ref_flux_table='ref_flux.
     # y axis of RFM = time-dependent flux
     RFM = table['ref_flux']
 
-    # number of reference stars = width of RFM
-    nrefs = RFM.shape[1]
+    # 1D RFM visualization
+    if mode in ['simple', 'full']:
+        visualize_1D_RFM(table=table, mode='norm', visu_dir=visu_dir, ylim=ylim, layout=layout)
+    elif mode == 'raw':
+        visualize_1D_RFM(table=table, mode='raw', visu_dir=visu_dir, ylim=ylim, layout=layout)
 
-    nrows, ncols = layout[0], layout[1]
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(28, 20))
-    fig.suptitle('Reference Stars Lightcurves', fontsize=30, y=0.92)
-    for nrow in range(nrows):
-        for ncol in range(ncols):
-            # n = reference star number
-            n = nrow * ncols + ncol
-            if n < nrefs:
-                if mode in ['simple', 'full']:
-                    # normalize flux for each reference star = normalize RFM by column median
-                    norm_nflux = RFM[:, n] / np.median(RFM[:, n])
-                    # set the same ylim for all normalized flux plots
-                    axes[nrow][ncol].set_ylim(ylim)
-                    # scatter normalized flux write original median flux to legend
-                    axes[nrow][ncol].scatter(table.time.datetime64, norm_nflux, 
-                                    s=10, marker='x', color='k', linewidth=0.5, 
-                                    label='ref'+str(n+1)+', median≈'+str(int(round(np.median(RFM[:, n]),-2))))
-                    # smooth normalized flux by a Savitzky-Golay rolling mean of deg=1
-                    smooth_nflux = savgol_filter(norm_nflux, window_length=51, polyorder=1)
-                    # plot smoothed normalized flux curve in red
-                    axes[nrow][ncol].plot(table.time.datetime64, smooth_nflux, 
-                                    markersize=0, marker='.', color='tab:red', linewidth=2)
-            elif mode == 'raw':
-                # scatter raw flux for each reference star
-                axes[nrow][ncol].scatter(table.time.datetime64, RFM[:, n], 
-                                s=10, marker='x', color='k', linewidth=0.5, label='ref'+str(n+1))
-                # smooth raw flux by a Savitzky-Golay rolling mean of deg=1
-                smooth_nflux = savgol_filter(RFM[:, n], window_length=51, polyorder=1)
-                # plot smoothed raw flux curve in red
-                axes[nrow][ncol].plot(table.time.datetime64, smooth_nflux, 
-                                markersize=0, marker='.', color='tab:red', linewidth=2)
-            # format plot and axis labels
-            axes[nrow][ncol].grid(True)
-            axes[nrow][ncol].legend(loc='upper left', fontsize=14)
-            axes[nrow][ncol].xaxis.set_major_locator(MaxNLocator(7))
-            axes[nrow][ncol].xaxis.set_major_formatter(
-                                    mdates.ConciseDateFormatter(axes[nrow][ncol].xaxis.get_major_locator()))
-    plt.savefig(os.path.join(visu_dir, 'reference_stars_lc.pdf'))
-    mpld3.save_html(fig, os.path.join(visu_dir, 'reference_stars_lc.html'))
-    plt.close()
-
-
-    # if we transpose RFM, its row = each reference star; column = time-dependent flux
-    # the transposed RFM is refered to as 'spectrum'
-    # the wavelength axis of a normal spectrum is now a time axis for the RFM spectrum
-    spectrum = RFM.transpose()
-
-    # sort spectrum by row median (median flux for each reference star) in descending order; get sort_index
-    sort_index = np.median(spectrum, axis=1).argsort()[::-1]
-    # normalize spectrum by row
-    normalized_spectrum = sknorm(spectrum, axis=1)
-    sorted_spec = normalized_spectrum[sort_index]
-    sorted_spec = sorted_spec * (1 / np.median(sorted_spec))
-
-    if mode == 'full':
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))
-        fig.suptitle('2D Reference Flux Map')
-        # plot the flux-sorted spectrum as an image
-        # set spectrum color range = ylim for normalized reference star lc
-        im1 = ax.imshow(sorted_spec, cmap='viridis', aspect='auto', 
-                        interpolation='None', vmin=ylim[0], vmax=ylim[1])
-        fig.colorbar(im1, ax=ax, location='right', aspect=30, pad=0.03, 
-                            label='normalized flux')
-
-        # configure yticks and yticklabels to represent correct reference star number
-        ax.set_yticks(np.arange(start=0, stop=nrefs))
-        labels = [str(n+1) for n in sort_index]
-        labels = ['ref'+m for m in labels]
-        ax.set_yticklabels(labels, fontsize=8)
-        ax.set_ylabel('reference star number  - - - median flux + + +')
-        ax.set_xlabel('Time', fontsize=10, labelpad=10)
-        ax.tick_params(labelbottom=False)
-
-        # tight layout and output png
-        plt.tight_layout()
-        plt.savefig(os.path.join(visu_dir, '2d_RFM.png'), dpi=1200)
-        plt.close()
-
-
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(16,12))
-    fig.suptitle('3D Reference Flux Map', y=0.92, fontsize=16)
-
-    X = np.arange(start=1, stop=nrefs+1, step=1)
-    Y = mdates.date2num(table.time.datetime64)
-
-    for n in sort_index:
-        # n = reference star number
-        norm_nflux = RFM[:, n] / np.median(RFM[:, n])
-        smooth_nflux = savgol_filter(norm_nflux, window_length=51, polyorder=1)
-        Xlocator = sort_index.tolist().index(n)
-        if n == sort_index[0]:
-            ax.plot(np.linspace(start=Xlocator,stop=Xlocator,num=RFM.shape[0]), Y, smooth_nflux, 
-                c='red', linewidth=0.8, alpha=0.8, label='Savitzky-Golay smoothing')
-        else:
-            ax.plot(np.linspace(start=Xlocator,stop=Xlocator,num=RFM.shape[0]), Y, smooth_nflux, 
-                c='red', linewidth=0.8, alpha=0.8)
-    ax.w_yaxis.set_major_locator(MaxNLocator(7))
-    ax.w_yaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.w_yaxis.get_major_locator()))
-
-    # configure yticks and yticklabels to represent correct reference star number
-    ax.set_xticks(np.arange(start=0, stop=nrefs))
-    labels = [str(n+1) for n in sort_index]
-    ax.set_xticklabels(labels, fontsize=8)
-    ax.set_ylabel('DateTime', fontsize=10, labelpad=10)
-    ax.set_xlabel('Reference Star Number  + + + median flux - - -', labelpad=10)
-    ax.set_zlabel('Normalized Flux', labelpad=10)
-
-    if mode == 'full':
-        X, Y = np.meshgrid(X, Y)
-        Z = sorted_spec.transpose()
-        ax.set_zlim(ylim[0], ylim[1])
-        norm = Normalize(vmin=ylim[0], vmax=ylim[1])
-        surf = ax.scatter(X-1, Y, Z, c=Z, s=1, norm=norm, cmap='viridis', linewidths=0, alpha=1, label='raw flux')
-        fig.colorbar(surf, ax=ax, location='left', shrink=0.5, aspect=30, label='normalized flux')
-    
-    ax.legend()
-    plt.savefig(os.path.join(visu_dir, '3d_RFM.pdf'), dpi=1200)
-    plt.show()
+    # 2D and 3D RFM visualization
+    if mode in ['simple', 'raw']:
+        visualize_3D_RFM(table=table, mode='simple', visu_dir=visu_dir, ylim=ylim)
+    elif mode == 'full':
+        visualize_2D_RFM(table=table, visu_dir=visu_dir, ylim=ylim)
+        visualize_3D_RFM(table=table, mode='full scatter', visu_dir=visu_dir, ylim=ylim)
 
     return table, RFM
 
--- visu_functions.py
+++ visu_functions.py
@@ -1,5 +1,9 @@
 import os
 
+import numpy as np
+from scipy.signal import savgol_filter
+from sklearn.preprocessing import normalize as sknorm
+
 import mpld3
 import matplotlib.pylab as plt
 from matplotlib.patches import Rectangle, Circle
@@ -172,3 +176,190 @@ def visualize_detection(img, masked_img, skymap, table,
     plt.show()
 
     return
+
+
+
+def visualize_1D_RFM(table, mode, visu_dir, ylim=[0.85, 1.15], layout=[4, 6]):
+    """
+    Visualize reference stars' lightcurves in traditional time-flux subplots.
+    Please see phot_functions.py for an explanation of RFM
+    """
+    assert mode in ['norm', 'raw']
+
+    RFM = table['ref_flux']
+    # number of reference stars = width of RFM
+    nrefs = RFM.shape[1]
+
+    nrows, ncols = layout[0], layout[1]
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(28, 20))
+    fig.suptitle('Reference Stars Lightcurves', fontsize=30, y=0.92)
+    for nrow in range(nrows):
+        for ncol in range(ncols):
+            # n = reference star number
+            n = nrow * ncols + ncol
+            if n < nrefs:
+                if mode == 'norm':
+                    # normalize flux for each reference star = normalize RFM by column median
+                    norm_nflux = RFM[:, n] / np.median(RFM[:, n])
+                    # set the same ylim for all normalized flux plots
+                    axes[nrow][ncol].set_ylim(ylim)
+                    # scatter normalized flux write original median flux to legend
+                    axes[nrow][ncol].scatter(table.time.datetime64, norm_nflux, 
+                                s=10, marker='x', color='k', linewidth=0.5, 
+                                label='ref'+str(n+1)+', median≈'+str(int(round(np.median(RFM[:, n]),-2))))
+                    # smooth normalized flux by a Savitzky-Golay rolling mean of deg=1
+                    smooth_nflux = savgol_filter(norm_nflux, window_length=51, polyorder=1)
+                    # plot smoothed normalized flux curve in red
+                    axes[nrow][ncol].plot(table.time.datetime64, smooth_nflux, 
+                                markersize=0, marker='.', color='tab:red', linewidth=2)
+                elif mode == 'raw':
+                    # scatter raw flux for each reference star
+                    axes[nrow][ncol].scatter(table.time.datetime64, RFM[:, n], 
+                                s=10, marker='x', color='k', linewidth=0.5, label='ref'+str(n+1))
+                    # smooth raw flux by a Savitzky-Golay rolling mean of deg=1
+                    smooth_nflux = savgol_filter(RFM[:, n], window_length=51, polyorder=1)
+                    # plot smoothed raw flux curve in red
+                    axes[nrow][ncol].plot(table.time.datetime64, smooth_nflux, 
+                                markersize=0, marker='.', color='tab:red', linewidth=2)
+                # format plot and axis labels
+                axes[nrow][ncol].grid(True)
+                axes[nrow][ncol].legend(loc='upper left', fontsize=14)
+                axes[nrow][ncol].xaxis.set_major_locator(MaxNLocator(7))
+                axes[nrow][ncol].xaxis.set_major_formatter(
+                                mdates.ConciseDateFormatter(axes[nrow][ncol].xaxis.get_major_locator()))
+    # save in pdf and in html (interactive)
+    plt.savefig(os.path.join(visu_dir, 'reference_stars_lc.pdf'))
+    mpld3.save_html(fig, os.path.join(visu_dir, 'reference_stars_lc.html'))
+    plt.close()
+
+    return
+
+
+
+def visualize_2D_RFM(table, visu_dir, ylim=[0.85, 1.15]):
+    """
+    Visualize reference stars' lightcurves in a 2D image: x-axis=time, y-axis=nref, color=flux
+    Please see phot_functions.py for an explanation of RFM
+    """
+    RFM = table['ref_flux']
+    nrefs = RFM.shape[1]
+
+    # if we transpose RFM, its row = each reference star; column = time-dependent flux
+    # the transposed RFM is refered to as 'spectrum'
+    # the wavelength axis of a normal spectrum is now a time axis for the RFM spectrum
+    spectrum = RFM.transpose()
+    # sort spectrum by row median (median flux for each reference star) in descending order; get sort_index
+    sort_index = np.median(spectrum, axis=1).argsort()[::-1]
+    # normalize spectrum by row
+    normalized_spectrum = sknorm(spectrum, axis=1)
+    sorted_spec = normalized_spectrum[sort_index]
+    sorted_spec = sorted_spec * (1 / np.median(sorted_spec))
+
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))
+    fig.suptitle('2D Reference Flux Map')
+    # plot the flux-sorted spectrum as an image
+    # set spectrum color range = ylim for normalized reference star lc
+    im1 = ax.imshow(sorted_spec, cmap='viridis', aspect='auto', 
+                    interpolation='None', vmin=ylim[0], vmax=ylim[1])
+    fig.colorbar(im1, ax=ax, location='right', aspect=30, pad=0.03, 
+                        label='normalized flux')
+
+    # configure yticks and yticklabels to represent correct reference star number
+    ax.set_yticks(np.arange(start=0, stop=nrefs))
+    labels = [str(n+1) for n in sort_index]
+    labels = ['ref'+m for m in labels]
+    ax.set_yticklabels(labels, fontsize=8)
+    ax.set_ylabel('reference star number  - - - median flux + + +')
+    ax.set_xlabel('Time', fontsize=10, labelpad=10)
+    ax.tick_params(labelbottom=False)
+
+    # tight layout and output png
+    plt.tight_layout()
+    plt.savefig(os.path.join(visu_dir, '2d_RFM.png'), dpi=1200)
+    plt.close()
+
+    return
+
+
+
+def visualize_3D_RFM(table, mode, visu_dir, ylim):
+    """
+    Visualize reference stars' lightcurves in 3D. 
+    Please see phot_functions.py for an explanation of RFM
+    """
+    assert mode in ['simple', 'full scatter']
+    
+    # initialization; please see visualize_2D_RFM for explanation
+    RFM = table['ref_flux']
+    nrefs = RFM.shape[1]
+    spectrum = RFM.transpose()
+    sort_index = np.median(spectrum, axis=1).argsort()[::-1]
+    normalized_spectrum = sknorm(spectrum, axis=1)
+    sorted_spec = normalized_spectrum[sort_index]
+    sorted_spec = sorted_spec * (1 / np.median(sorted_spec))
+
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(16,12))
+    fig.suptitle('3D Reference Flux Map', y=0.92, fontsize=16)
+
+    # X-axis: reference star number. the np.arange here is a locator; does not correlate to ref number
+    # Y-axis: mpl num conversion of np.datetime64 timestamps
+    X = np.arange(start=1, stop=nrefs+1, step=1)
+    Y = mdates.date2num(table.time.datetime64)
+
+    # loop through reference stars according to sort order, brightest first
+    for n in sort_index:
+        # n = reference star number
+        # normalize each star's flux by its median flux
+        norm_nflux = RFM[:, n] / np.median(RFM[:, n])
+        # smooth normalized flux by a Savitzky-Golay rolling mean of deg=1
+        smooth_nflux = savgol_filter(norm_nflux, window_length=51, polyorder=1)
+        # reinitialize an X-axis locator for each star to plot on specific x
+        Xlocator = sort_index.tolist().index(n)
+        if n == sort_index[0]:
+            # plot smoothed curve for brightest star with label
+            ax.plot(np.linspace(start=Xlocator,stop=Xlocator,num=RFM.shape[0]), Y, smooth_nflux, 
+                c='red', linewidth=0.8, alpha=0.8, label='Savitzky-Golay smoothing')
+        else:
+            # plot smoothed curve for nth brightest star one by one
+            ax.plot(np.linspace(start=Xlocator,stop=Xlocator,num=RFM.shape[0]), Y, smooth_nflux, 
+                c='red', linewidth=0.8, alpha=0.8)
+    # set Y-axis format to concise DateTime; limit max=7 time labels for Y-axis
+    ax.w_yaxis.set_major_locator(MaxNLocator(7))
+    ax.w_yaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.w_yaxis.get_major_locator()))
+
+    # configure xticks and xticklabels to represent correct reference star number
+    ax.set_xticks(np.arange(start=0, stop=nrefs))
+    labels = [str(n+1) for n in sort_index]
+    ax.set_xticklabels(labels, fontsize=8)
+
+    # set XYZ axes labels
+    ax.set_ylabel('DateTime', fontsize=10, labelpad=10)
+    ax.set_xlabel('Reference Star Number  + + + median flux - - -', labelpad=10)
+    ax.set_zlabel('Normalized Flux', labelpad=10)
+
+    # scatter raw flux data points if mode == full scatter
+    if mode == 'full scatter':
+        # meshgrid for X, Y to get img shape; correlation: one (x,y) pair --> one z
+        X, Y = np.meshgrid(X, Y)
+        Z = sorted_spec.transpose()
+
+        # set zlim (flux/display color) to the same as 1D ylim and 2D color vmin-vmax
+        ax.set_zlim(ylim[0], ylim[1])
+        # normalize colorbar
+        norm = Normalize(vmin=ylim[0], vmax=ylim[1])
+        # scatter raw flux, color --> z, disable depth of field (alpha=1) for performance speed
+        surf = ax.scatter(X-1, Y, Z, c=Z, s=1, norm=norm, cmap='viridis', 
+                            linewidths=0, alpha=1, label='raw flux')
+        fig.colorbar(surf, ax=ax, location='left', shrink=0.5, aspect=30, label='normalized flux')
+    
+    # legend and save pdf
+    ax.legend()
+    plt.savefig(os.path.join(visu_dir, '3d_RFM.pdf'), dpi=1200)
+    plt.show()
+    
+    return
+
+
+
