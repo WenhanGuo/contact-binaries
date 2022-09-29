3c66d3a6a854fc435a0bcc95ef20da47c5b22ae4
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-09-28 23:20:36 -0700

    added multi filter calib

--- PPP.py
+++ PPP.py
@@ -1,18 +1,18 @@
 # %%
 import os
-from essentials import avg_cube, calibrate_cube, solve_and_align
+from essentials import avg_cube, calibrate_flat, calibrate_cube, solve_and_align
 from glob import glob1
 
 
-directory = '/Users/danny/Mirror/ASTRO/Contact_Binary/data/CSS_034852/cgri_test'
+directory = '/Users/danny/Mirror/ASTRO/ASTR101/lab2/data'
 cali_dir='/Users/danny/Mirror/ASTRO/Calibration/CALIBS_20220921'
 cubelist = sorted(glob1(directory, '*.fits'))
 
 
 # %%
 # For creating sigma-clipped master frames
-'''
-avg_cube(directory=cali_dir, 
+
+'''avg_cube(directory=cali_dir, 
         cubename='Darks_20220921_001521.fits',
         outname='Master_Dark_20220921.fits')
 avg_cube(directory=cali_dir, 
@@ -29,17 +29,41 @@ avg_cube(directory=cali_dir,
         outname='Master_Flat_r_20220921.fits')
 avg_cube(directory=cali_dir, 
         cubename='twilights_1i_20220920_183559.fits',
-        outname='Master_Flat_i_20220921.fits')
-'''
+        outname='Master_Flat_i_20220921.fits')'''
+
+
+# %%
+# calibrate master flats by dark, bias, and normalization
+calibrate_flat(cali_dir=cali_dir, 
+                flatname='Master_Flat_C_20220921.fits',
+                darkname='Master_Dark_20220921.fits',
+                biasname='Master_Bias_20220921.fits')
+calibrate_flat(cali_dir=cali_dir, 
+                flatname='Master_Flat_g_20220921.fits',
+                darkname='Master_Dark_20220921.fits',
+                biasname='Master_Bias_20220921.fits')
+calibrate_flat(cali_dir=cali_dir, 
+                flatname='Master_Flat_r_20220921.fits',
+                darkname='Master_Dark_20220921.fits',
+                biasname='Master_Bias_20220921.fits')
+calibrate_flat(cali_dir=cali_dir, 
+                flatname='Master_Flat_i_20220921.fits',
+                darkname='Master_Dark_20220921.fits',
+                biasname='Master_Bias_20220921.fits')
+
 
 # %%
 for cubename in cubelist:
     calibrate_cube(directory=directory, cubename=cubename, 
-                    cali_dir='/Users/danny/Mirror/ASTRO/JPL_NEO/Calibration', 
-                    biasname='master_bias_20220726.fits', 
-                    darkname='master_dark_20220726.fits', 
+                    cali_dir=cali_dir, 
+                    biasname='Master_Bias_20220921.fits', 
+                    darkname='Master_Dark_20220921.fits', 
                     darkexptime=10.0, 
-                    flatname='master_flat11_20220713.fits', 
+                    mode='multicolor',
+                    Cflat='Master_Flat_C_20220921_calibrated.fits',
+                    gflat='Master_Flat_g_20220921_calibrated.fits',
+                    rflat='Master_Flat_r_20220921_calibrated.fits',
+                    iflat='Master_Flat_i_20220921_calibrated.fits',
                     out_dir=os.path.join(directory, 'reduced_cubes'))
 
 
