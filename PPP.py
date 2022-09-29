a218c0b4740e41675ffe00c8fcb76827cce88d64
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-09-28 21:40:26 -0700

    added avg_cube in PPP

--- PPP.py
+++ PPP.py
@@ -1,14 +1,38 @@
 # %%
 import os
-from essentials import calibrate_cube, solve_and_align
+from essentials import avg_cube, calibrate_cube, solve_and_align
 from glob import glob1
-import astropy.units as u
 
 
-directory = '/Users/danny/Mirror/ASTRO/JPL_NEO/Contact_Binary/data/CSS_034852/20220719'
+directory = '/Users/danny/Mirror/ASTRO/Contact_Binary/data/CSS_034852/cgri_test'
+cali_dir='/Users/danny/Mirror/ASTRO/Calibration/CALIBS_20220921'
 cubelist = sorted(glob1(directory, '*.fits'))
 
 
+# %%
+# For creating sigma-clipped master frames
+'''
+avg_cube(directory=cali_dir, 
+        cubename='Darks_20220921_001521.fits',
+        outname='Master_Dark_20220921.fits')
+avg_cube(directory=cali_dir, 
+        cubename='Biases_20220921_004356.fits',
+        outname='Master_Bias_20220921.fits')
+avg_cube(directory=cali_dir, 
+        cubename='twilights_7C_20220920_190121.fits',
+        outname='Master_Flat_C_20220921.fits')
+avg_cube(directory=cali_dir, 
+        cubename='twilights_4g_20220920_185345.fits',
+        outname='Master_Flat_g_20220921.fits')
+avg_cube(directory=cali_dir, 
+        cubename='twilights_2r_20220920_184356.fits',
+        outname='Master_Flat_r_20220921.fits')
+avg_cube(directory=cali_dir, 
+        cubename='twilights_1i_20220920_183559.fits',
+        outname='Master_Flat_i_20220921.fits')
+'''
+
+# %%
 for cubename in cubelist:
     calibrate_cube(directory=directory, cubename=cubename, 
                     cali_dir='/Users/danny/Mirror/ASTRO/JPL_NEO/Calibration', 
