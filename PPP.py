63ab82151c063da13cabd8178e6486231e9bb442
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-09-30 16:24:56 -0700

    uses darkbias and normalized master flat

--- PPP.py
+++ PPP.py
@@ -1,80 +1,85 @@
 # %%
 import os
-from essentials import avg_cube, calibrate_flat, calibrate_cube, solve_and_align
+from essentials import *
 from glob import glob1
 
 
 directory = '/Users/danny/Mirror/ASTRO/ASTR101/lab2/data'
+# directory = '/Users/danny/Mirror/ASTRO/Contact_Binary/data/CSS_034852/cgri_test'
 cali_dir='/Users/danny/Mirror/ASTRO/Calibration/CALIBS_20220921'
 cubelist = sorted(glob1(directory, '*.fits'))
 
 
 # %%
-# For creating sigma-clipped master frames
+# For creating sigma-clipped master frames from calibration cubes
 
-'''avg_cube(directory=cali_dir, 
+avg_cube(directory=cali_dir, 
         cubename='Darks_20220921_001521.fits',
-        outname='Master_Dark_20220921.fits')
+        outname='Master_DarkBias.fits')
 avg_cube(directory=cali_dir, 
         cubename='Biases_20220921_004356.fits',
-        outname='Master_Bias_20220921.fits')
+        outname='Master_Bias.fits')
 avg_cube(directory=cali_dir, 
         cubename='twilights_7C_20220920_190121.fits',
-        outname='Master_Flat_C_20220921.fits')
+        outname='Master_Flat_C.fits')
 avg_cube(directory=cali_dir, 
         cubename='twilights_4g_20220920_185345.fits',
-        outname='Master_Flat_g_20220921.fits')
+        outname='Master_Flat_g.fits')
 avg_cube(directory=cali_dir, 
         cubename='twilights_2r_20220920_184356.fits',
-        outname='Master_Flat_r_20220921.fits')
+        outname='Master_Flat_r.fits')
 avg_cube(directory=cali_dir, 
         cubename='twilights_1i_20220920_183559.fits',
-        outname='Master_Flat_i_20220921.fits')'''
+        outname='Master_Flat_i.fits')
 
 
 # %%
-# calibrate master flats by dark, bias, and normalization
+# calibrate master dark and master flats
+
 calibrate_flat(cali_dir=cali_dir, 
-                flatname='Master_Flat_C_20220921.fits',
-                darkname='Master_Dark_20220921.fits',
-                biasname='Master_Bias_20220921.fits')
+                flatname='Master_Flat_C.fits',
+                darkbiasname='Master_DarkBias.fits')
 calibrate_flat(cali_dir=cali_dir, 
-                flatname='Master_Flat_g_20220921.fits',
-                darkname='Master_Dark_20220921.fits',
-                biasname='Master_Bias_20220921.fits')
+                flatname='Master_Flat_g.fits',
+                darkbiasname='Master_DarkBias.fits')
 calibrate_flat(cali_dir=cali_dir, 
-                flatname='Master_Flat_r_20220921.fits',
-                darkname='Master_Dark_20220921.fits',
-                biasname='Master_Bias_20220921.fits')
+                flatname='Master_Flat_r.fits',
+                darkbiasname='Master_DarkBias.fits')
 calibrate_flat(cali_dir=cali_dir, 
-                flatname='Master_Flat_i_20220921.fits',
-                darkname='Master_Dark_20220921.fits',
-                biasname='Master_Bias_20220921.fits')
+                flatname='Master_Flat_i.fits',
+                darkbiasname='Master_DarkBias.fits')
 
 
 # %%
 for cubename in cubelist:
     calibrate_cube(directory=directory, cubename=cubename, 
                     cali_dir=cali_dir, 
-                    biasname='Master_Bias_20220921.fits', 
-                    darkname='Master_Dark_20220921.fits', 
+                    darkbiasname='Master_DarkBias.fits', 
                     darkexptime=10.0, 
                     mode='multicolor',
-                    Cflat='Master_Flat_C_20220921_calibrated.fits',
-                    gflat='Master_Flat_g_20220921_calibrated.fits',
-                    rflat='Master_Flat_r_20220921_calibrated.fits',
-                    iflat='Master_Flat_i_20220921_calibrated.fits',
+                    Cflat='Master_Flat_C_norm.fits',
+                    gflat='Master_Flat_g_norm.fits',
+                    rflat='Master_Flat_r_norm.fits',
+                    iflat='Master_Flat_i_norm.fits',
                     out_dir=os.path.join(directory, 'reduced_cubes'))
 
 
 # %%
 
+"""
 reduced_cubelist = sorted(glob1(os.path.join(directory, 'reduced_cubes'), '*_reduced.fits'))
 
 for cubename in reduced_cubelist: 
     solve_and_align(directory=os.path.join(directory, 'reduced_cubes'), 
                     cubename=cubename, 
                     out_dir=os.path.join(directory, 'aligned_cubes'))
+"""
+
+
+# %%
+batch_solve_and_align(directory=os.path.join(directory, 'reduced_cubes'), 
+                        out_dir=os.path.join(directory, 'aligned_cubes'),
+                        do_convolve=False)
 
 
 # %%
