84caea9cb0ae16fc65dc1289ca48490107b61327
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-11-21 14:47:17 -0800

    phoebe flow initial commit

---
+++ phoebe_flow.py
@@ -0,0 +1,9 @@
+### http://phoebe-project.org/docs/2.4/physics
+
+Danny -- 1,3   Nhi -- 2,4
+
+
+
+## requiv@primary, requiv@secondary, pot, fillout_factor
+http://phoebe-project.org/docs/2.4/tutorials/requiv_crit_contact
+
