845edab2ecbd126ed4f04faa6679d8e555c9b1bc
WenhanGuo <wgaa2021@mymail.pomona.edu>
2022-11-21 15:33:50 -0800

    phoebe flow basic structure

--- phoebe_flow.py
+++ phoebe_flow.py
@@ -1,9 +1,45 @@
-### http://phoebe-project.org/docs/2.4/physics
+##  http://phoebe-project.org/docs/2.4/physics
 
-Danny -- 1,3   Nhi -- 2,4
+## -------------------------------------- 1. System Effects --------------------------------------
+Systemic Velocity
+Rømer & Light Travel Time Effects
+Third Light
+Distance
+Extinction
 
+## ------------------------------------- 2. Orbital Effects -------------------------------------
+Various t0s
+Eccentricity & Volume Conservation
+Apsidal Motion
+Period Change
+Misalignment
+Rømer & Light Travel Time Effects
+Beaming & Boosting
 
+## ------------------------------------- 3. Stellar Effects -------------------------------------
+Equivalent Radius
+Potentials
+Critical Radii: Detached Systems
+Critical Radii: Semidetached Systems
+Critical Radii: [requiv@primary] [requiv@secondary] [pot] [fillout_factor]
+    http://phoebe-project.org/docs/2.4/tutorials/requiv_crit_contact
+Surface Gravities
+Eccentricity & Volume Conservation
+Spots
+Eclipse Detection
+Passband Luminosity
+Gravity Brightening/Darkening
+Reflection & Heating
+Reflection & Heating: Lambert Scattering
+Radial Velocity Offsets
 
-## requiv@primary, requiv@secondary, pot, fillout_factor
-http://phoebe-project.org/docs/2.4/tutorials/requiv_crit_contact
-
+## --------------------------- 4. Passband/Atmosphere/Dataset Effects ---------------------------
+Passbands & Atmospheres
+Passband Luminosity
+Limb Darkening
+Third Light
+Gravitational Redshift
+Radial Velocity Offsets
+Intensity Weighting
+Finite Time of Integration
+Gaussian Processes
